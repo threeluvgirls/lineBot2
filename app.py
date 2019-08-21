331888a756bcf628d47a4d201f627c30d304bf3b
threeluvgirls <54095877+threeluvgirls@users.noreply.github.com>
2019-08-21 14:05:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,14 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult]['intent']['displayName']
+    if (userText == 'สวัสดี') :
+        sendText(user,'ดีจ้า มีอะไรให้ช่วยมั้ย')
+    elif (userText == 'มี') :
+        sendText(user,'อยากรู้เรื่องไหนดีล่ะ')
+    else :
+        sendText(user,',มีหน่อยเถอะนะะ')
     return '',200
 
 def sendText(user, text):
